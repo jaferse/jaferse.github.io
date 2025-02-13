69710734b8d115018da0ae32cc3968cfc4b497cf
jaferse <157239043+jaferse@users.noreply.github.com>
2025-02-13 13:47:52 +0100

    Modificacion letra, header y parte del footer

--- js/animacionLogo.js
+++ js/animacionLogo.js
@@ -1,8 +1,8 @@
-let logo = document.querySelector('.header__logo');
-let logo__circulo = document.querySelector('.header__logo__circulo');
-let logo__Titulo = document.querySelector('.header__logo__circulo__Titulo');
-let logo__Eslogan = document.querySelector('.header__logo__circulo__Eslogan');
-let header__contenerdor = document.querySelector('.header__contenerdor__cita');
+// let logo = document.querySelector('.header__logo');
+// let logo__circulo = document.querySelector('.header__logo__circulo');
+// let logo__Titulo = document.querySelector('.header__logo__circulo__Titulo');
+// let logo__Eslogan = document.querySelector('.header__logo__circulo__Eslogan');
+// let header__contenerdor = document.querySelector('.header__contenerdor__cita');
 let ContainerSpinner = document.querySelector('.Container');
 
 /**
@@ -10,11 +10,11 @@ let ContainerSpinner = document.querySelector('.Container');
  */
 document.addEventListener('DOMContentLoaded', () => {
     ContainerSpinner.style.visibility='hidden';
-    logo.classList.add('animate--girar');
-    logo__circulo.classList.add('animate--encoger');
-    logo__Titulo.classList.add('animate--letras');
-    logo__Eslogan.classList.add('animate--letras');
-    header__contenerdor.classList.add('animate--contenedor');
+    // logo.classList.add('animate--girar');
+    // logo__circulo.classList.add('animate--encoger');
+    // logo__Titulo.classList.add('animate--letras');
+    // logo__Eslogan.classList.add('animate--letras');
+    // header__contenerdor.classList.add('animate--contenedor');
 
 
 })
